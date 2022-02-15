c3e5609ef61fcf641e7d90735a63a077c88568e0
Erik Dell <erikdell@gmail.com>
2022-02-15 11:50:30 -0300

    warning fix

--- springboot-service-item/src/main/java/com/dellmdq/springboot/app/item/models/service/ItemServiceFeign.java
+++ springboot-service-item/src/main/java/com/dellmdq/springboot/app/item/models/service/ItemServiceFeign.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Primary;
+//import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 import com.dellmdq.springboot.app.item.clients.ProductRestClient;
