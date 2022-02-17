4a892410622b754d6b0ffff7f5f8045288320076
Erik Dell <erikdell@gmail.com>
2022-02-16 21:13:12 -0300

    logger added

--- springboot-service-products/src/main/java/com/dellmdq/springboot/app/products/controller/ProductController.java
+++ springboot-service-products/src/main/java/com/dellmdq/springboot/app/products/controller/ProductController.java
@@ -4,8 +4,7 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
